3f3f12a4493c57176ddbc32325d35a4b466406dd
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2025-11-02 14:16:03 +0600

    getx controller updated

--- lib/feature/authenticaton/controller/onboarding_coltroller.dart
+++ lib/feature/authenticaton/controller/onboarding_coltroller.dart
@@ -1,9 +1,44 @@
 
+import 'package:fast_food/feature/authenticaton/screen/login&signup/login_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OnboardingColtroller extends  GetxController{
 
-  RxInt selectedPage = 0.obs;
+  static OnboardingColtroller get instance => Get.find();
+
+  final pageController = PageController();
+
+  RxInt currentIndex = 0.obs;
+
+   // update current indext when page scroll
+  void updatePageIndicator (index){
+    currentIndex.value= index;
+  }
+
+ // update pagen on dot click
+ 
+ void dotNavigationClick(index){
+
+  currentIndex.value=index;
+
+  pageController.jumpTo(index);
+
+ }
+ //// next page
+  void nextPage (){
+
+    currentIndex.value < 2
+        ? currentIndex.value++
+        : Get.offAll(LoginScreen());
+    pageController.jumpToPage(currentIndex.value);
+  }
+  //  jump to the last page
+  void skipPage(){
+    currentIndex.value =2;
+    pageController.jumpToPage(currentIndex.value);
+  } 
+
 
   
 
