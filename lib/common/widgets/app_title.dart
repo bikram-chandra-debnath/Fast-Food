41c2d35dc3258f8c73c953757c4fff98e51f8229
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-02-17 23:32:31 +0600

    Update Apptitle

--- lib/common/widgets/app_title.dart
+++ lib/common/widgets/app_title.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 
 class AppTitle extends StatelessWidget {
   const AppTitle({
-    super.key, required this.title,  this.textAlign=TextAlign.left, required this.style,
+    super.key, required this.title,  this.textAlign=TextAlign.left, required this.style, this.maxLines,
   });
 
   final String title;
   final TextAlign textAlign;
   final TextStyle style;
+  final int? maxLines;
 
   @override
   Widget build(BuildContext context) {
     return Text(
+      maxLines: maxLines,
       overflow: TextOverflow.fade,
       title,
       style: style,
