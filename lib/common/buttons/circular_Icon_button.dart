bf88f9768d2514f14126c18b40f8955e7960cf5a
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-02-21 04:22:33 +0600

    update voidCallBack

--- lib/common/buttons/circular_Icon_button.dart
+++ lib/common/buttons/circular_Icon_button.dart
@@ -2,36 +2,39 @@ import 'package:flutter/material.dart';
 
 import '../../utlis/constrant/app_colors.dart';
 
-
 class AppCircularIconButton extends StatelessWidget {
   const AppCircularIconButton({
     super.key,
     required this.icon,
     this.backgroundColor = AppColors.darkerGrey,
     this.color,
-    required this.onPressed,  this.width=45,  this.height=45, this.tapTargetSize, this.padding, this.minimumSize,
+     this.onPressed,
+    this.width = 45,
+    this.height = 45,
+    this.tapTargetSize,
+    this.padding,
+    this.minimumSize,
   });
 
   final Widget icon;
   final Color backgroundColor;
   final Color? color;
-  final VoidCallback onPressed;
-  final double width,height;
- final EdgeInsetsGeometry? padding;
- final Size? minimumSize;
-final MaterialTapTargetSize? tapTargetSize;
+  final VoidCallback? onPressed;
+  final double width, height;
+  final EdgeInsetsGeometry? padding;
+  final Size? minimumSize;
+  final MaterialTapTargetSize? tapTargetSize;
   @override
   Widget build(BuildContext context) {
     return IconButton(
       style: IconButton.styleFrom(
-        minimumSize:minimumSize ,
+        minimumSize: minimumSize,
         padding: padding,
 
-        
         backgroundColor: backgroundColor,
         fixedSize: Size(width, height),
         foregroundColor: color,
-        tapTargetSize: tapTargetSize
+        tapTargetSize: tapTargetSize,
       ),
       onPressed: onPressed,
       icon: icon,
