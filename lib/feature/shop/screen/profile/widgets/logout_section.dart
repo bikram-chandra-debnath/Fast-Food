343ffdf7670014b9809049d7e3d31cdd0d0bb73d
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-02-28 23:42:38 +0600

    Logout navigation

--- lib/feature/shop/screen/profile/widgets/logout_section.dart
+++ lib/feature/shop/screen/profile/widgets/logout_section.dart
@@ -1,9 +1,11 @@
 
 import 'package:fast_food/common/shape/rounded_container.dart';
 import 'package:fast_food/common/widgets/profile/custom_list_tile.dart';
+import 'package:fast_food/feature/authenticaton/screen/login/login_screen.dart';
 import 'package:fast_food/utlis/constrant/app_image.dart';
 import 'package:fast_food/utlis/constrant/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LogoutSection extends StatelessWidget {
   const LogoutSection({
@@ -16,7 +18,7 @@ class LogoutSection extends StatelessWidget {
       child: AppCustomListTile(
         title: AppText.logout,
         leadingIcon: AppImage.logoutRed,
-        ontap: () {},
+        ontap: () => Get.offAll(()=> LoginScreen()),
       ),
     );
   }
