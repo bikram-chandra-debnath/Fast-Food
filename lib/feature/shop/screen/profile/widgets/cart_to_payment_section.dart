5f92913ffdf1a53cfb6e9297fdfc5cf4998067a7
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-02-28 19:34:52 +0600

    update cart navigatin

--- lib/feature/shop/screen/profile/widgets/cart_to_payment_section.dart
+++ lib/feature/shop/screen/profile/widgets/cart_to_payment_section.dart
@@ -1,9 +1,11 @@
 
 import 'package:fast_food/common/shape/rounded_container.dart';
 import 'package:fast_food/common/widgets/profile/custom_list_tile.dart';
+import 'package:fast_food/feature/shop/screen/cart/cart_screen.dart';
 import 'package:fast_food/utlis/constrant/app_image.dart';
 import 'package:fast_food/utlis/constrant/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartToPaymentSection extends StatelessWidget {
   const CartToPaymentSection({
@@ -18,7 +20,7 @@ class CartToPaymentSection extends StatelessWidget {
           AppCustomListTile(
             title: AppText.cart,
             leadingIcon: AppImage.cartBlue,
-            ontap: () {},
+            ontap: () => Get.to(()=> CartScreen()),
           ),
           AppCustomListTile(
             title: AppText.favorite,
